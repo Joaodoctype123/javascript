8cf7b0da5449e4d9c3106a821b662f4e8ce570ac
Joaodoctype123 <joaogabriel576mi@gmail.com>
2025-11-02 20:51:23 +0000

    update

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel >= 62) { // Condição simples!
+    console.log(`Velocidade não permitida. REPROVADO !`)
+} else {
+    console.log(`Velocidade permitida. APROVADO !`)
+}
+console.log(`Dirija sempre usando o cinto de segurança !`)
+
+
+// console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
